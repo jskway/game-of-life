6da29952c4be428baffe16f8243ac416ad202eab
jackskim <jackskim29@gmail.com>
2020-06-20 18:12:48 -0700

    Set up components

---
+++ src/components/Controls.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Controls() {
+  return (
+    <section>
+      <h3>Controls</h3>
+    </section>
+  );
+}
+
+export default Controls;
---
+++ src/components/Grid.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Grid() {
+  return (
+    <section>
+      <h3>Grid</h3>
+    </section>
+  );
+}
+
+export default Grid;
