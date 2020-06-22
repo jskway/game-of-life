72d3529f9e8c9642b66e0de01eb374f022cc8d6f
jackskim <jackskim29@gmail.com>
2020-06-21 18:03:00 -0700

    Set up controls

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,9 +1,24 @@
 import React from "react";
 
-function Controls() {
+function Controls({ isRunning, setIsRunning }) {
+  const start = () => {
+    setIsRunning(true);
+  };
+
+  const stop = () => {
+    setIsRunning(false);
+  };
+
   return (
     <section>
       <h3>Controls</h3>
+      <div>
+        {isRunning ? (
+          <button onClick={stop}>Stop</button>
+        ) : (
+          <button onClick={start}>Start</button>
+        )}
+      </div>
     </section>
   );
 }
