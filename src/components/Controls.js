89fc5e4f6174e1b2a09de56935b3705d1dc8cba1
jackskim <jackskim29@gmail.com>
2020-06-24 18:55:37 -0700

    Change buttons to be disabled while the sim is running

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -8,6 +8,7 @@ function Controls({ isRunning, setIsRunning, rows, cols, setGeneration }) {
   const { grid, setGrid } = useContext(GridContext);
   const { setLiveCells } = useContext(CellContext);
   const [timeoutId, setTimeoutId] = useState(null);
+  const [ preset, setPreset ] = useState("none");
 
   const start = () => {
     setIsRunning(true);
@@ -36,6 +37,10 @@ function Controls({ isRunning, setIsRunning, rows, cols, setGeneration }) {
     const randomGrid = makeRandomGrid(rows, cols);
     setGrid(randomGrid);
     setLiveCells(getLiveCells(rows, cols, randomGrid));
+  };
+
+  const changePreset = (e) => {
+    setPreset(e.target.value);
   };
 
   const makeGenerationOnce = (grid, cols, rows) => {
@@ -104,9 +109,13 @@ function Controls({ isRunning, setIsRunning, rows, cols, setGeneration }) {
         ) : (
           <button onClick={start}>Start</button>
         )}
-        <button onClick={next}>Next</button>
-        <button onClick={clear}>Clear</button>
-        <button onClick={random}>Random</button>
+        <button disabled={isRunning ? true : false} onClick={next}>Next</button>
+        <button disabled={isRunning ? true : false} onClick={clear}>Clear</button>
+        <button disabled={isRunning ? true : false} onClick={random}>Random</button>
+        <select disabled={isRunning ? true : false} onChange={changePreset} value={preset}>
+          <option value="none">Select a Sample Configuration</option>
+          <option value="glider">Glider</option>
+        </select>
       </div>
     </section>
   );
